ab92cbc4e04d5fe0da1adb5767760da3c771a64b
EmmaKD <edogbevi@gmail.com>
2014-02-25 12:52:54 -0500

    reviewed javascript file

--- week4.js
+++ week4.js
@@ -3,21 +3,82 @@
  */
 console.log("hi there");
 
-function googleLoaded(){}
+//UNEMDATA is the local name of the json file I just loaded
+function dataLoaded(UNEMPDATA) {
 
-function pageLoad(){
-		
+	console.log(UNEMPDATA);
+
+	var myObsData = UNEMPDATA.observations;
+
+	//I am trying to construct an array of arrays
+	var myDataArray = [];
+	//console.log(myDataArray);
+	//wait a second, I need headers to be the first
+	//
 	
-console.log("go to page loaded");
+	var headerArray = ["Date", "value"];
+	
+	myDataArray.push(headerArray);
+	
 
-//load the google visualization library
-google.load("visualization",'1', {packages: ["corechart"], callback: "googleLoaded"});	
+	//specify starting point and ending point
+	for (var i = 0; i < myObsData.length; i++) {
 
-//
+		//create reference to current object in list
+		var currObj = myObsData[i]
 
-$.get("UEMP270V_data")
+		var currArray = [currObj.date, Number(currObj.value)];
+
+		myDataArray.push(currArray);
+
+		//end for loop
+	}
+	
+	console.log(myDataArray);
+	
+	
+	//feed data to visualization library
+	var myDataTable = google.visualization.arrayToDataTable(myDataArray);
+	
+	//create options object to actually customize the look if the chart
+	
+	var options = {
+          title: 'Unemployment rate'
+        };
+
+	
+	//tell it to create a line chart, and give it the 
+	var myChart = new google.visualization.LineChart(document.getElementById("MyChart"));
+	myChart.draw(myDataTable);
+}
+
+function googleLoaded() {
+	
+	console.log("googleLoaded");
+	
+	$.get("UNEMP270V_data.json", dataLoaded, "json");
 	
 }
+
+console.log("google loaded");
+
+function pageLoad() {
+
+	console.log("go to page loaded");
+
+	//load the google visualization library
+	google.load("visualization", '1', {
+		packages : ["corechart"],
+		callback : "googleLoaded"
+	}); 
+	
+
+	//
+
+	
+
+}
+
 
 $(document).ready(pageLoad);
 
